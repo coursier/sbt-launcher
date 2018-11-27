68d735dc480ca531d301edc7bfb0b295292680bb
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-11-27 18:01:16 +0100

    Get coursier launcher from generate-csbt.sh if needed

--- generate-csbt.sh
+++ generate-csbt.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 VERSION="${VERSION:-1.1.0-M1}"
 
-coursier bootstrap \
+if which coursier >/dev/null 2>&1; then
+  COURSIER=coursier
+else
+  COURSIER="target/coursier"
+  if [ ! -e "$COURSIER" ]; then
+    mkdir -p "$(dirname "$COURSIER")"
+    curl -Lo "$COURSIER" https://github.com/coursier/coursier/raw/862c977a9cfc59dc84743ac05863a454ed199860/coursier
+    chmod +x "$COURSIER"
+  fi
+fi
+
+"$COURSIER" bootstrap \
   "io.get-coursier:sbt-launcher_2.12:$VERSION" \
   "io.get-coursier:coursier-okhttp_2.12:1.1.0-M7" \
   -r central \
